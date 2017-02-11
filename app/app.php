67d7e7e5a270d5c92af1220a9f5c8709d2d25df6
Sarah Leahy <sleahy115@gmail.com>
2017-02-10 16:24:21 -0800

    correct spacing and formatting

--- app/app.php
+++ app/app.php
@@ -7,32 +7,27 @@
         'twig.path' => __DIR__.'/../views'
     ));
     session_start();
+
     if (empty($_SESSION['list_of_contacts'])) {
     $_SESSION['list_of_contacts'] = array();
     }
     $app["debug"] = true;
 
-    $app->get("/", function() use ($app)
-    {
+    $app->get("/", function() use ($app){
       return $app['twig']->render("input_form.html.twig", array("contacts" => Contact::getAll()));
-    });
-    $app->post("/book", function() use ($app)
-    {
+      });
+    $app->post("/book", function() use ($app){
       $new_contact = new Contact($_POST['name'], $_POST['address'], $_POST['phone-number'], $_POST['email']);
       $new_contact->save();
       $new_contact->clearContact($new_contact);
       return $app['twig']->render("form_output.html.twig", array("contact_list"=>$_SESSION['list_of_contacts']));
-
     //   $delete_contact = array_slice($_SESSION['list_of_contacts'], -1);
     //   $delete_contact = array();
     //   var_dump($delete_contact);
-
     });
-    $app->get("/clear", function() use ($app)
-    {
+    $app->get("/clear", function() use ($app){
         return $app['twig']->render("clear.html.twig", array("clear" => Contact::clearAll()));
     });
-
     // $app->get("/clear-contact", function() use ($app)
     // {
     //
